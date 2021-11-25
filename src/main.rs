a322e1c5719ad1541a7da8db532a7cb651e9818b
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-11-26 01:15:15 +0900

    Hmm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Deserialize;
 use std::sync::Mutex;
+
+#[derive(Deserialize)]
+struct Info {
+    username: String,
+}
 
 struct AppStateWithCounter {
     counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
@@ -24,8 +30,22 @@ async fn main() -> std::io::Result<()> {
             // Note: using app_data instead of data
             .app_data(counter.clone()) // <- register the created data
             .route("/", web::get().to(index))
+            .route("/foo", web::post().to(foo))
+            .service(echo)
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+// working; test with `curl -X POST http://127.0.0.1:8080/echo -d "user=user1&pass=abcd"`
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+// not working?
+async fn foo(info: web::Json<Info>) -> Result<String> {
+    println!("Welcome {}!", info.username);
+    Ok(format!("Welcome {}!", info.username))
 }
